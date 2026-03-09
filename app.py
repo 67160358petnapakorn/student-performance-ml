54555294c74f6b6350c88974b2cddbb97375a7ac
67160358petnapakorn <67160358@go.buu.ac.th>
2026-03-10 01:26:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,33 @@
 import streamlit as st
 import joblib
-import numpy as np
+import pandas as pd
 
 st.title("🎓 Student Performance Predictor")
 
 @st.cache_resource
-def load_model():
+def load_artifacts():
     model = joblib.load("student_score_model.pkl")
-    return model
+    features = joblib.load("features.pkl")
+    return model, features
 
-model = load_model()
+model, features = load_artifacts()
 
-st.write("Enter student study information")
+st.write("Enter student information")
 
 study_hours = st.slider("Study Hours", 0, 10, 5)
 attendance = st.slider("Attendance (%)", 0, 100, 80)
 sleep_hours = st.slider("Sleep Hours", 0, 10, 7)
 previous_score = st.slider("Previous Score", 0, 100, 60)
 
-input_data = np.array([[study_hours, attendance, sleep_hours, previous_score]])
+input_dict = {
+    features[0]: study_hours,
+    features[1]: attendance,
+    features[2]: sleep_hours,
+    features[3]: previous_score
+}
+
+input_df = pd.DataFrame([input_dict])
 
 if st.button("Predict Score"):
-    prediction = model.predict(input_data)
-
+    prediction = model.predict(input_df)
     st.success(f"📊 Predicted Score: {prediction[0]:.2f}")
